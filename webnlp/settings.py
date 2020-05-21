2a6a1d1a07b5b05f7af44765702f0570993210e9
Andres Insuasty <31679261+AndresInsuasty@users.noreply.github.com>
2020-05-21 16:42:32 -0500

    Settings parameters

--- webnlp/settings.py
+++ webnlp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=j$t!n1^l6knput*l+&i+b())nsn4rc1#t5o9g-gjx4wl^@i@k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-3-22-233-102.us-east-2.compute.amazonaws.com','3.22.233.102','localhost']
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
